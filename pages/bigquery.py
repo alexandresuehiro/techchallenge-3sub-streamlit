e6fa964017c8a0ade0a2d47a561f6487e7a5fe34
alexandresuehiro <107832355+alexandresuehiro@users.noreply.github.com>
2025-05-15 23:41:20 +0000

    inserting more changes

---
+++ pages/bigquery.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objs as go
+from utils.constantes import TITULO_BIGQUERY, TITULO_PRINCIPAL
+from utils.layout import output_layout
+
+st.set_page_config(
+    page_title=f"{TITULO_BIGQUERY} | {TITULO_PRINCIPAL}",
+    layout="wide",
+)
+output_layout()
+
+with st.container():
+            st.header(f":orange[Usando o Bigquery]")
+            st.subheader(":blue[Queries usadas]", divider="blue")
+            st.markdown(
+                """
+    
+    
+                """)
+
+
