cc19a25ff04ecde7056abc3fb422bc63788f91ee
alexandresuehiro <107832355+alexandresuehiro@users.noreply.github.com>
2025-05-19 20:44:27 +0000

    more changes

--- pages/indicadores.py
+++ pages/indicadores.py
@@ -5,14 +5,7 @@ import plotly.graph_objs as go
 from utils.constantes import TITULO_INDICADORES, TITULO_PRINCIPAL
 from utils.layout import output_layout, format_number
 
-from tabs.indicadores.IAN_tab import SobreIAN
-from tabs.indicadores.IDA_tab import SobreIDA
-from tabs.indicadores.IEG_tab import SobreIEG
-from tabs.indicadores.IAA_tab import SobreIAA
-from tabs.indicadores.IPS_tab import SobreIPS
-from tabs.indicadores.IPP_tab import SobreIPP
-from tabs.indicadores.IPV_tab import SobreIPV
-from tabs.indicadores.INDE_tab import SobreINDE
+
 from tabs.tab import TabInterface
 
 
@@ -28,21 +21,22 @@ with st.container():
 
         st.markdown(
             """
-            Nesta sessão de análise, detalharemos sobre os Indicadores de Avaliação que compõem o INDE:
+            Nesta sessão de análise, detalharemos sobre alguns sintomas do Covid-19, registrados na base do Pnad Covid-19:
 
-            Dimensão acadêmica: 
-            *	IAN: Indicador de Adequação de Nível (Indicador de Avaliação)
-            *	IDA: Indicador de Desempenho Acadêmico (Indicador de Avaliação)
-            *   IEG: Indicador de Engajamento (Indicador de Avaliação)
-
-            
-            Dimensão psicossocial:
-            *   IAA: Indicador de Autoavaliação (Indicador de Avaliação)
-            *   IPS: Indicador Psicossocial (Indicador de Conselho)
-
-            Dimensão psicopedagógica:
-            *   IPP: Indicador Psicopedagógico (Indicador de Conselho)
-            *   IPV: Indicador de Ponto de Virada (Indicador de Conselho)
+            *	Febre
+            *	Tosse
+            *   Dor de garganta
+            *   Dificuldade de respirar
+            *   Dor de cabeça
+            *   Dor no peito
+            *   Náusea
+            *   Nariz entupido ou escorrendo
+            *   Fadiga
+            *   Dor nos olhos
+            *   Pedra de olfato ou paladar
+            *   Dor muscular
+            *   Febre, Tosse e dificuldade de respirar
+            *   Febre, Tosse e dor no peito
         """
         )
         # # Criando 8 expanders
@@ -80,25 +74,5 @@ with st.container():
         #     with st.expander(f"IPV"):
         #         chamarIPV = SobreIPV()
         #         st.write(chamarIPV)
-        tab0, tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(
-             tabs=[
-                 "INDE",
-                 "IAA",
-                 "IAN",
-                 "IDA",
-                 "IEG",
-                 "IPS",
-                 "IPP",
-                 "IPV"
-             ]
-         )
-
-        SobreINDE(tab0)
-        SobreIAA(tab1)
-        SobreIAN(tab2)
-        SobreIDA(tab3)
-        SobreIEG(tab4)
-        SobreIPS(tab5)
-        SobreIPP(tab6)
-        SobreIPV(tab7)
+       
     
